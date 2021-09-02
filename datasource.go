5aace94d7d8bd65aebc2d1980bfc8d7e6403bf7e
Thomas Schenker <tommzn@gmx.de>
2021-09-02 22:47:28 +0200

    update comments got golint

--- datasource.go
+++ datasource.go
@@ -1,15 +1,18 @@
 package weather
 
 import (
+	config "github.com/tommzn/go-config"
 	core "github.com/tommzn/hdb-datasource-core"
 	events "github.com/tommzn/hdb-events-go"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func New() core.DataSource {
+// New returns a new weather datasource
+func New(config config.Config) core.DataSource {
 	return &WeatherDataSource{}
 }
 
+// Fetch returns current weather for coordinates defined by given config
 func (datasource *WeatherDataSource) Fetch() (interface{}, error) {
 	return &events.Weather{
 		Timestamp:   timestamppb.Now(),
--- types.go
+++ types.go
@@ -1,4 +1,6 @@
 package weather
 
+// WeatherDataSource fetches current weather information from Open Weather Map
+// for a location defined by given config.
 type WeatherDataSource struct {
 }
