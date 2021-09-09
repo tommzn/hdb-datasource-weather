dd8ddcc6123590b7c53ec0ac74d7f2dc71f75da2
Thomas Schenker <tommzn@gmx.de>
2021-09-10 00:23:56 +0200

    add lambda function handler

---
+++ lambda/go.mod
@@ -0,0 +1,12 @@
+module github.com/tommzn/hdb-datasource-weather/lambda
+
+go 1.16
+
+require (
+	github.com/aws/aws-lambda-go v1.26.0
+	github.com/tommzn/go-config v1.0.5
+	github.com/tommzn/go-log v1.0.2
+	github.com/tommzn/go-secrets v1.0.0
+	github.com/tommzn/hdb-datasource-core v1.0.1
+	github.com/tommzn/hdb-datasource-weather v1.0.2
+)
---
+++ lambda/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+
+	config "github.com/tommzn/go-config"
+	log "github.com/tommzn/go-log"
+	secrets "github.com/tommzn/go-secrets"
+
+	core "github.com/tommzn/hdb-datasource-core"
+	weather "github.com/tommzn/hdb-datasource-weather"
+)
+
+func main() {
+
+	collector, err := bootstrap()
+	if err != nil {
+		panic(err)
+	}
+	lambda.Start(collector.Run)
+}
+
+// bootstrap loads config and creates a new scheduled collector with a weather datasource.
+func bootstrap() (core.Collector, error) {
+
+	conf := loadConfig()
+	secretsManager := newSecretsManager()
+	logger := newLogger(conf, secretsManager)
+	datasource, err := weather.New(conf, secretsManager)
+	if err != nil {
+		return nil, err
+	}
+
+	queue := conf.Get("hdb.queue", config.AsStringPtr("de.tsl.hdb.weather"))
+	return core.NewScheduledCollector(*queue, datasource, conf, logger), nil
+}
+
+// loadConfig from config file.
+func loadConfig() config.Config {
+
+	configSource, err := config.NewS3ConfigSourceFromEnv()
+	if err != nil {
+		panic(err)
+	}
+
+	conf, err := configSource.Load()
+	if err != nil {
+		panic(err)
+	}
+	return conf
+}
+
+// newSecretsManager retruns a new secrets manager from passed config.
+func newSecretsManager() secrets.SecretsManager {
+	return secrets.NewSecretsManager()
+}
+
+// newLogger creates a new logger from  passed config.
+func newLogger(conf config.Config, secretsMenager secrets.SecretsManager) log.Logger {
+	return log.NewLoggerFromConfig(conf, secretsMenager)
+}
